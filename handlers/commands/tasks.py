80548e008d3d762ee5d95759e26fad45af389773
revenuemediabot <Mircles1@gmail.com>
2025-06-08 14:36:01 +0300

    Update tasks.py

--- handlers/commands/tasks.py
+++ handlers/commands/tasks.py
@@ -1,28 +1,24 @@
-from telegram import Update
+# handlers/commands/tasks.py
+
 from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import Update
 
 async def tasks_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # TODO: Вывести список задач пользователя
-    await update.message.reply_text("Ваш список задач здесь...")
+    await update.message.reply_text("Ваш список задач...")
 
 async def addtask_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # TODO: Добавить задачу
     await update.message.reply_text("Добавлена новая задача!")
 
 async def settasks_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # TODO: Установить список задач
     await update.message.reply_text("Список задач обновлён.")
 
 async def edit_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # TODO: Редактирование задач
-    await update.message.reply_text("Редактирование задачи...")
+    await update.message.reply_text("Редактирование задач...")
 
 async def reset_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # TODO: Сбросить задачи
     await update.message.reply_text("Список задач сброшен.")
 
 async def addsub_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # TODO: Добавить подзадачу
     await update.message.reply_text("Подзадача добавлена.")
 
 def register_task_handlers(application: Application):
