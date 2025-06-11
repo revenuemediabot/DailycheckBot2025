de878d5df60d6a71c7aca5d15f822feded6ebdcb
revenuemediabot <Mircles1@gmail.com>
2025-06-11 13:53:04 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,17 @@
-# models/__init__.py
+"""
+DailyCheck Bot v4.0 - Модели данных
+Все классы данных проекта
+"""
+
+from .user import User, UserSettings, UserStats
+from .task import Task, TaskCompletion, Subtask
+from .achievement import Achievement, AchievementSystem
+from .reminder import Reminder
+from .friend import Friend
+
+__all__ = [
+    'User', 'UserSettings', 'UserStats',
+    'Task', 'TaskCompletion', 'Subtask', 
+    'Achievement', 'AchievementSystem',
+    'Reminder', 'Friend'
+]
